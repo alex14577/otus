75ea88581595e191f6afbe08e1c9e2786fa5caf6
Alex <alexanc40@gmail.com>
2023-03-11 12:11:45 +0300

    Исправления по ревью

--- hw4/print_ip/print_ip.h
+++ hw4/print_ip/print_ip.h
@@ -8,75 +8,53 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <array>
+
+template<typename T>
+std::array<uint8_t, sizeof(T)> Split(T n)
+{
+    std::array<uint8_t, sizeof(T)> res;
+    for(size_t i = sizeof(T) -1; i >= 0; i++)
+    {
+        res[i] = (n >> (8 * i)) & 0xff;
+    }
+    return res;
+}
 
 template<typename T>
 void PrintIp(T ip)
 {
     if constexpr(std::is_integral<T>::value)
     {
-        if constexpr(is_int8_t<T>::value)
+        auto arr = Split(ip);
+        for(size_t i = 0; i < arr.size(); ++i)
         {
-            std::cout << std::to_string( static_cast<uint8_t>(ip) );
-        }
-        if constexpr(is_int16_t<T>::value)
-        {
-            uint8_t ip1 = ip & 0xff00 >> 8;
-            uint8_t ip2 = ip & 0x00ff >> 0;
-            std::cout   << std::to_string( static_cast<uint8_t>(ip1) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip2) );
-        }
-        if constexpr(is_int32_t<T>::value)
-        {
-            uint8_t ip1 = (ip & 0xff000000) >> 24;
-            uint8_t ip2 = (ip & 0x00ff0000) >> 16;
-            uint8_t ip3 = (ip & 0x0000ff00) >> 8;
-            uint8_t ip4 = (ip & 0x000000ff) >> 0;
-            std::cout   << std::to_string( static_cast<uint8_t>(ip1) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip2) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip3) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip4) );
-        }
-        if constexpr(is_int64_t<T>::value)
-        {
-            uint8_t ip1 = (ip & 0xff00000000000000) >> 56;
-            uint8_t ip2 = (ip & 0x00ff000000000000) >> 48;
-            uint8_t ip3 = (ip & 0x0000ff0000000000) >> 40;
-            uint8_t ip4 = (ip & 0x000000ff00000000) >> 32;
-            uint8_t ip5 = (ip & 0x00000000ff000000) >> 24;
-            uint8_t ip6 = (ip & 0x0000000000ff0000) >> 16;
-            uint8_t ip7 = (ip & 0x000000000000ff00) >> 8;
-            uint8_t ip8 = (ip & 0x00000000000000ff) >> 0;
-            std::cout   << std::to_string( static_cast<uint8_t>(ip1) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip2) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip3) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip4) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip5) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip6) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip7) ) << '.' 
-                        << std::to_string( static_cast<uint8_t>(ip8) );
+            if(i != 0)
+            {
+                std::cout << '.';
+            }
+            std::cout << std::to_string(arr[i]);
         }
     }
-    
-    if constexpr(is_string<T>::value)
+    else if constexpr(std::is_same<T, std::string>::value)
     {
         std::cout << ip;
     }
-
-    if constexpr(is_vector<T>::value || is_list<T>::value)
+    else if constexpr(  std::is_same<T, std::vector<typename T::value_type>>::value ||
+                        std::is_same<T, std::list<typename T::value_type>>::value)
     {
-        if(ip.empty())
+        bool afterFirstPart = false;
+        for(const auto& part : ip )
         {
-            return;
-        }
-
-        auto it = ip.cbegin();
-        std::cout << std::to_string( *it );
-
-        ++it;
-        while(it != ip.cend())
-        {
-            std::cout << '.' << std::to_string( *it );
-            ++it;
+            if(afterFirstPart)
+            {  
+                std::cout << '.';
+            }
+            else
+            {
+                afterFirstPart = true;    
+            }
+            std::cout << std::to_string( part);
         }
     }
 }
--- hw4/test/src/test_print_ip.cpp
+++ hw4/test/src/test_print_ip.cpp
@@ -1,76 +1,34 @@
+#include <print_ip/print_ip.h>
 
 #include <iostream>
 #include <string>
 #include <gtest/gtest.h>
 
-class TestResult
-{
-public:
-    TestResult() = default;
-
-    void AddRes(const std::string& res)
-    {
-        res_ += res;
-    }
-
-    void AddRes(const char res)
-    {
-        res_ += res;
-    }
-
-    std::string& GetRes()
-    {
-        return res_;
-    }
-
-    void Reset()
-    {
-        res_.clear();
-    }
-
-private:
-    std::string res_;
-} testResult;
-
-std::ostream&  operator << (std::ostream &os, const std::basic_string<char> &s)
-{
-    testResult.AddRes(s);
-    return os;
-}
-std::ostream&  operator << (std::ostream &os, const char s)
-{
-    testResult.AddRes(s);
-    return os;
-}
-
-std::ostream&  operator << (std::ostream &os, const unsigned int s)
-{
-    std::string str{std::to_string( s )};
-    testResult.AddRes(str);
-    return os;
-}
-
 class PrintIpTest : public ::testing::Test
 {
 protected:
-	void SetUp()
-	{
-        testResult.Reset();
-	}
-	void TearDown()
-	{
-
-	}
+    void SetUp() {
+        prev = std::cout.rdbuf(buffer.rdbuf());
+    }
+    void TearDown() {
+        std::cout.rdbuf(prev);
+        std::stringstream{}.swap(buffer); // reset buffer state with flags and etc.
+    }
+    std::string output() const {
+        return buffer.str();
+    }
+private:
+    std::streambuf* prev{};
+    std::stringstream buffer;
 };
 
-#include <print_ip/print_ip.h>
 
 TEST_F(PrintIpTest, TestipInt8)
 {
     int8_t ipInt8 = -1;
     PrintIp(ipInt8);
 
-    EXPECT_EQ(testResult.GetRes(), std::string("255"));    
+    EXPECT_EQ(output(), "255");    
 }
 
 TEST_F(PrintIpTest, TestipInt16)
@@ -78,7 +36,7 @@ TEST_F(PrintIpTest, TestipInt16)
     int16_t ipInt16 = 0;
     PrintIp(ipInt16);
 
-    EXPECT_EQ(testResult.GetRes(), std::string("0.0"));    
+    EXPECT_EQ(output(), "0.0");    
 }
 
 TEST_F(PrintIpTest, TestipInt32)
@@ -86,7 +44,7 @@ TEST_F(PrintIpTest, TestipInt32)
     int32_t ipInt32 = 2130706433;
     PrintIp(ipInt32);
 
-    EXPECT_EQ(testResult.GetRes(), std::string("127.0.0.1"));    
+    EXPECT_EQ(output(), "127.0.0.1");    
 }
 
 TEST_F(PrintIpTest, TestipInt64)
@@ -94,7 +52,7 @@ TEST_F(PrintIpTest, TestipInt64)
     int64_t ipInt64 = 8875824491850138409;
     PrintIp(ipInt64);
 
-    EXPECT_EQ(testResult.GetRes(), std::string("123.45.67.89.101.112.131.41"));    
+    EXPECT_EQ(output(), "123.45.67.89.101.112.131.41");
 }
 
 TEST_F(PrintIpTest, TestipString)
@@ -103,7 +61,7 @@ TEST_F(PrintIpTest, TestipString)
     std::string ipString {ipVal};
     PrintIp(ipString);
 
-    EXPECT_EQ(testResult.GetRes(), std::string( ipVal ));    
+    EXPECT_EQ(output(), ipVal);  
 }
 
 TEST_F(PrintIpTest, TestipIntVec)
@@ -111,7 +69,7 @@ TEST_F(PrintIpTest, TestipIntVec)
     std::vector<int> ipVec = {400, 300, 200, 100};
     PrintIp(ipVec);
 
-    EXPECT_EQ(testResult.GetRes(), std::string("400.300.200.100"));    
+    EXPECT_EQ(output(), "400.300.200.100");
 }
 
 TEST_F(PrintIpTest, TestipInt)
@@ -119,5 +77,5 @@ TEST_F(PrintIpTest, TestipInt)
     std::list<short> ipList = {400, 300, 200, 100};
     PrintIp(ipList);
 
-    EXPECT_EQ(testResult.GetRes(), std::string("400.300.200.100"));    
+    EXPECT_EQ(output(), "400.300.200.100");
 }
